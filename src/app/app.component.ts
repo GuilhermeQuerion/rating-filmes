846830d215a58ef3398fe55e779c326b9c29542b
GuilhermeQuerion <guilhermequerion@icloud.com>
2022-03-26 16:47:39 -0300

    first-commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  movies = [
+    { title: 'Os Vingadores', rating: 5 },
+    { title: 'Joias Brutas', rating: 3 },
+    { title: 'Parasita', rating: 5 },
+    { title: 'O Irlandês', rating: 4 },
+    { title: 'Pantera Negra', rating: 4 },
+  ];
+}
---
+++ src/app/panel/panel.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'panel',
+  templateUrl: './panel.component.html',
+  styleUrls: ['./panel.component.css']
+})
+export class PanelComponent {
+
+  expanded = true;
+  @Input() title!: string;
+
+  expand() {
+    this.expanded = true;
+  }
+
+  contract() {
+    this.expanded = false;
+  }
+}
